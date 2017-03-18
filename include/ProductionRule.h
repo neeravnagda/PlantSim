0721e49235100fe3155787e5f5d0735f14cc9b6f
neeravnagda <neerav24@hotmail.com>
2017-03-18 19:35:16 +0000

    created struct ProductionRule

---
+++ include/ProductionRule.h
@@ -0,0 +1,25 @@
+#ifndef PRODUCTIONRULE_H
+#define PRODUCTIONRULE_H
+
+#include <string>
+
+/// @file ProductionRule.h
+/// @brief this struct allows the L-system production rules to be put in a custom container
+/// @author Neerav Nagda
+/// @version 1.0
+/// @date 18/03/17
+/// @struct ProductionRule
+/// @brief struct to contain information about the L-system production rules
+
+typedef struct ProductionRule
+{
+		std::string m_predecessor;	///< Character(s) to be replaced
+		std::string m_successor;	///< Character(s) to replace
+		float m_probability;	///< Probability of replacing predecessor with successor
+		ProductionRule(std::string _predecessor, std::string _successor, float _probability) :
+				m_predecessor(_predecessor),
+				m_successor(_successor),
+				m_probability(_probability){}
+} ProductionRule;
+
+#endif // PRODUCTIONRULE_H
